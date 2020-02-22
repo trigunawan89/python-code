0b447f8ae76eaddec0a4a8f672d946c13e700f64
trigunawan89 <trigun.bsns@gmail.com>
2020-02-22 21:54:40 +0700

    closure

---
+++ closure.py
@@ -0,0 +1,10 @@
+def outerFunction(text):
+    text = text
+
+    def innerFunction():
+        print(text)
+    return innerFunction
+
+if __name__ == '__main__':
+    myFunction = outerFunction('Hey!')
+    myFunction()
